9e9dbfd038a2f1886e61208ecd21fb502cba5480
Brian Cosgrove <cosgroveb@gmail.com>
2012-01-19 22:01:52 -0600

    adds src directory

---
+++ src/jane_web_app.erl
@@ -0,0 +1,17 @@
+%% @doc Callbacks for the jane_web application.
+
+-module(jane_web_app).
+
+-behaviour(application).
+-export([start/2,stop/1]).
+
+
+%% @spec start(_Type, _StartArgs) -> ServerRet
+%% @doc application start callback for jane_web.
+start(_Type, _StartArgs) ->
+    jane_web_sup:start_link().
+
+%% @spec stop(_State) -> ServerRet
+%% @doc application stop callback for jane_web.
+stop(_State) ->
+    ok.
