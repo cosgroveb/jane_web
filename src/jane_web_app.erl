8bdde28f42f7aacf2f01a4be97b27fcb7f3c8d0c
Brian Cosgrove <cosgroveb@gmail.com>
2012-01-19 22:09:31 -0600

    starts mochiweb and webmachine

--- src/jane_web_app.erl
+++ src/jane_web_app.erl
@@ -9,9 +9,13 @@
 %% @spec start(_Type, _StartArgs) -> ServerRet
 %% @doc application start callback for jane_web.
 start(_Type, _StartArgs) ->
+    application:start(mochiweb),
+    application:start(webmachine),
     jane_web_sup:start_link().
 
 %% @spec stop(_State) -> ServerRet
 %% @doc application stop callback for jane_web.
 stop(_State) ->
+    application:stop(mochiweb),
+    application:stop(webmachine),
     ok.
